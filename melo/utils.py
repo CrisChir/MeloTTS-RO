a603b7c180ce5b0bbf30f3a75d41f279f725cdcf
CrisChir <cristian.chirita@gmail.com>
2025-10-01 16:33:52 +0300

    Update utils.py

--- melo/utils.py
+++ melo/utils.py
@@ -433,18 +433,3 @@ class HParams:
     def __repr__(self):
         return self.__dict__.__repr__()
 
-def distribute_phone(phone_len, word_len):
-    """
-    Distributes the phoneme count across the word's sub-tokens.
-    For example, if a word has 5 phonemes and 2 sub-tokens,
-    it might distribute them as [3, 2].
-    """
-    phones_per_word = phone_len // word_len
-    remaining_phones = phone_len % word_len
-    phone_distribution = []
-    for i in range(word_len):
-        phones = phones_per_word
-        if i < remaining_phones:
-            phones += 1
-        phone_distribution.append(phones)
-    return phone_distribution
