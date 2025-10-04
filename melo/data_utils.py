a691a50804955f3541d7aa9c3992336a52faed3c
CrisChir <cristian.chirita@gmail.com>
2025-10-04 13:19:07 +0300

    Update data_utils.py

--- melo/data_utils.py
+++ melo/data_utils.py
@@ -4,47 +4,82 @@ import torch
 import torch.utils.data
 from tqdm import tqdm
 from loguru import logger
+
+# --- THIS IS THE FIX ---
+# We add the missing imports that were causing the NameError and other issues.
 import commons
 from mel_processing import spectrogram_torch, mel_spectrogram_torch
-from utils import load_filepaths_and_text
-from utils import load_wav_to_torch_librosa as load_wav_to_torch
+import utils
 from text import cleaned_text_to_sequence, get_bert
-import numpy as np
+# --- END OF FIX ---
 
 """Multi speaker version"""
 
 class TextAudioSpeakerLoader(torch.utils.data.Dataset):
+    """
+    1) loads audio, speaker_id, text pairs
+    2) normalizes text and converts them to sequences of integers
+    3) computes spectrograms from audio files.
+    """
     def __init__(self, audiopaths_sid_text, hparams):
-        # This function now correctly receives the full hps object
+        # This is the correct __init__ function that expects the FULL hps object
         self.hparams = hparams
         self.audiopaths_sid_text = utils.load_filepaths_and_text(audiopaths_sid_text)
         self.max_wav_value = self.hparams.data.max_wav_value
         self.sampling_rate = self.hparams.data.sampling_rate
+        self.filter_length = self.hparams.data.filter_length
+        self.hop_length = self.hparams.data.hop_length
+        self.win_length = self.hparams.data.win_length
         self.spk_map = self.hparams.data.spk2id
+
+        # Correctly access model and data parameters from the full hps object
         self.disable_bert = getattr(self.hparams.model, "disable_bert", False)
-        # ... other initializations from your file
+        self.use_mel_spec_posterior = getattr(self.hparams.model, "use_mel_posterior_encoder", False)
+        if self.use_mel_spec_posterior:
+            self.n_mel_channels = getattr(self.hparams.data, "n_mel_channels", 80)
+        
+        self.cleaned_text = getattr(self.hparams.data, "cleaned_text", False)
+        self.add_blank = self.hparams.data.add_blank
+        self.min_text_len = getattr(self.hparams.data, "min_text_len", 1)
+        self.max_text_len = getattr(self.hparams.data, "max_text_len", 300)
+
         random.seed(1234)
         random.shuffle(self.audiopaths_sid_text)
         self._filter()
 
     def _filter(self):
-        # ... your filter logic here ...
-        pass
+        audiopaths_sid_text_new, lengths = [], []
+        skipped = 0
+        logger.info("Init dataset...")
+        for item in tqdm(self.audiopaths_sid_text):
+            try:
+                _id, spk, language, text, phones, tone, word2ph = item
+            except:
+                print(item); raise
+            audiopath = f"{_id}"
+            if self.min_text_len <= len(phones.split(" ")) and len(phones.split(" ")) <= self.max_text_len:
+                phones_list = phones.split(" ")
+                tone_list = [int(i) for i in tone.split(" ")]
+                word2ph_list = [int(i) for i in word2ph.split(" ")]
+                audiopaths_sid_text_new.append([audiopath, spk, language, text, phones_list, tone_list, word2ph_list])
+                lengths.append(os.path.getsize(audiopath) // (2 * self.hop_length))
+            else:
+                skipped += 1
+        logger.info(f'min: {min(lengths)}; max: {max(lengths)}' )
+        logger.info(f"skipped: {skipped}, total: {len(self.audiopaths_sid_text)}")
+        self.audiopaths_sid_text = audiopaths_sid_text_new
+        self.lengths = lengths
 
     def get_audio_text_speaker_pair(self, audiopath_sid_text):
-        # ... your logic here ...
-        # This now correctly remaps the bert path
-        bert = None
-        if not self.disable_bert:
-            bert_path = os.path.join('/kaggle/working/bert_features/', os.path.relpath(audiopath, '/kaggle/input/')).replace(".wav", ".bert.pt")
-            try: bert = torch.load(bert_path)
-            except: print(f"Could not load BERT for {audiopath}"); raise
-        # ... rest of your logic
+        audiopath, sid, language, text, phones, tone, word2ph = audiopath_sid_text
+        bert, ja_bert, phones, tone, language = self.get_text(text, word2ph, phones, tone, language, audiopath)
+        spec, wav = self.get_audio(audiopath)
+        sid = int(self.spk_map.get(sid, 0))
+        sid = torch.LongTensor([sid])
         return (phones, spec, wav, sid, tone, language, bert, ja_bert)
 
-
     def get_audio(self, filename):
-        audio_norm, sampling_rate = load_wav_to_torch(filename, self.sampling_rate)
+        audio_norm, sampling_rate = utils.load_wav_to_torch_librosa(filename, self.sampling_rate)
         if sampling_rate != self.sampling_rate:
             raise ValueError(f"{filename} {sampling_rate} SR doesn't match target {self.sampling_rate} SR")
         audio_norm = audio_norm.unsqueeze(0)
@@ -64,7 +99,6 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
             for i in range(len(word2ph)): word2ph[i] = word2ph[i] * 2
             word2ph[0] += 1
         
-        # --- FIX #1: Correctly calculate the BERT file path ---
         INPUT_DATA_PREFIX = '/kaggle/input/'
         OUTPUT_BERT_PREFIX = '/kaggle/working/bert_features/'
         relative_path = os.path.relpath(wav_path, INPUT_DATA_PREFIX)
@@ -86,7 +120,6 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
             if language_str in ["ZH"]:
                 bert = bert
                 ja_bert = torch.zeros(768, len(phone))
-            # --- FIX #2: Add 'RO' to the list of languages ---
             elif language_str in ["JP", "EN", "ZH_MIX_EN", "KR", 'SP', 'ES', 'FR', 'DE', 'RU', 'MS', 'RO']:
                 ja_bert = bert
                 bert = torch.zeros(1024, len(phone))
@@ -105,24 +138,15 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.audiopaths_sid_text)
 
-# The rest of the file (Collate and Sampler) is standard and correct
+# The rest of the file is standard and has no errors
 class TextAudioSpeakerCollate:
-    def __init__(self, return_ids=False):
-        self.return_ids = return_ids
+    def __init__(self, return_ids=False): self.return_ids = return_ids
     def __call__(self, batch):
         _, ids_sorted_decreasing = torch.sort(torch.LongTensor([x[1].size(1) for x in batch]), dim=0, descending=True)
-        max_text_len = max([len(x[0]) for x in batch])
-        max_spec_len = max([x[1].size(1) for x in batch])
-        max_wav_len = max([x[2].size(1) for x in batch])
-        text_lengths = torch.LongTensor(len(batch))
-        spec_lengths = torch.LongTensor(len(batch))
-        wav_lengths = torch.LongTensor(len(batch))
-        sid = torch.LongTensor(len(batch))
-        text_padded = torch.LongTensor(len(batch), max_text_len)
-        tone_padded = torch.LongTensor(len(batch), max_text_len)
-        language_padded = torch.LongTensor(len(batch), max_text_len)
-        bert_padded = torch.FloatTensor(len(batch), 1024, max_text_len)
-        ja_bert_padded = torch.FloatTensor(len(batch), 768, max_text_len)
+        max_text_len = max([len(x[0]) for x in batch]); max_spec_len = max([x[1].size(1) for x in batch]); max_wav_len = max([x[2].size(1) for x in batch])
+        text_lengths = torch.LongTensor(len(batch)); spec_lengths = torch.LongTensor(len(batch)); wav_lengths = torch.LongTensor(len(batch)); sid = torch.LongTensor(len(batch))
+        text_padded, tone_padded, language_padded = torch.LongTensor(len(batch), max_text_len), torch.LongTensor(len(batch), max_text_len), torch.LongTensor(len(batch), max_text_len)
+        bert_padded, ja_bert_padded = torch.FloatTensor(len(batch), 1024, max_text_len), torch.FloatTensor(len(batch), 768, max_text_len)
         spec_padded = torch.FloatTensor(len(batch), batch[0][1].size(0), max_spec_len)
         wav_padded = torch.FloatTensor(len(batch), 1, max_wav_len)
         text_padded.zero_(); tone_padded.zero_(); language_padded.zero_(); spec_padded.zero_(); wav_padded.zero_(); bert_padded.zero_(); ja_bert_padded.zero_()
