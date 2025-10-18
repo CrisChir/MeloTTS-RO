861686c7427fa00eb83f64bdf42f9b55a27cbdf5
CrisChir <cristian.chirita@gmail.com>
2025-10-18 21:39:58 +0300

    Update utils.py

--- melo/utils.py
+++ melo/utils.py
@@ -19,6 +19,61 @@ logger = logging.getLogger(__name__)
 
 
 
+# def get_text_for_tts_infer(text, language_str, hps, device, symbol_to_id=None):
+#     norm_text, phone, tone, word2ph = clean_text(text, language_str)
+#     phone, tone, language = cleaned_text_to_sequence(phone, tone, language_str, symbol_to_id)
+
+#     if hps.data.add_blank:
+#         phone = commons.intersperse(phone, 0)
+#         tone = commons.intersperse(tone, 0)
+#         language = commons.intersperse(language, 0)
+#         for i in range(len(word2ph)):
+#             word2ph[i] = word2ph[i] * 2
+#         word2ph[0] += 1
+
+#     # if getattr(hps.data, "disable_bert", False):
+#     #     bert = torch.zeros(1024, len(phone))
+#     #     ja_bert = torch.zeros(768, len(phone))
+#     # else:
+#     #     bert = get_bert(norm_text, word2ph, language_str, device)
+#     #     del word2ph
+#     #     assert bert.shape[-1] == len(phone), phone
+
+#     if getattr(hps.data, "disable_bert", False):
+#         bert = torch.zeros(1024, len(phone))
+#         ja_bert = torch.zeros(768, len(phone))
+#     else:
+#         # Move interspersing BEFORE BERT
+#         if hps.data.add_blank:
+#             phone = commons.intersperse(phone, 0)
+#             tone = commons.intersperse(tone, 0)
+#             language = commons.intersperse(language, 0)
+#             for i in range(len(word2ph)):
+#                 word2ph[i] = word2ph[i] * 2
+#             word2ph[0] += 1
+
+#     bert = get_bert(norm_text, word2ph, language_str, device)
+#     del word2ph
+#     assert bert.shape[-1] == len(phone), f"BERT len {bert.shape[-1]} vs phone len {len(phone)}"
+
+#         if language_str == "ZH":
+#             bert = bert
+#             ja_bert = torch.zeros(768, len(phone))
+#         elif language_str in ["JP", "EN", "ZH_MIX_EN", "KR", "SP", "ES", "FR", "DE", "RU", "RO"]:
+#             ja_bert = bert
+#             bert = torch.zeros(1024, len(phone))
+
+#         else:
+#             raise NotImplementedError()
+
+#     assert bert.shape[-1] == len(
+#         phone
+#     ), f"Bert seq len {bert.shape[-1]} != {len(phone)}"
+
+#     phone = torch.LongTensor(phone)
+#     tone = torch.LongTensor(tone)
+#     language = torch.LongTensor(language)
+#     return bert, ja_bert, phone, tone, language
 def get_text_for_tts_infer(text, language_str, hps, device, symbol_to_id=None):
     norm_text, phone, tone, word2ph = clean_text(text, language_str)
     phone, tone, language = cleaned_text_to_sequence(phone, tone, language_str, symbol_to_id)
@@ -31,44 +86,23 @@ def get_text_for_tts_infer(text, language_str, hps, device, symbol_to_id=None):
             word2ph[i] = word2ph[i] * 2
         word2ph[0] += 1
 
-    # if getattr(hps.data, "disable_bert", False):
-    #     bert = torch.zeros(1024, len(phone))
-    #     ja_bert = torch.zeros(768, len(phone))
-    # else:
-    #     bert = get_bert(norm_text, word2ph, language_str, device)
-    #     del word2ph
-    #     assert bert.shape[-1] == len(phone), phone
-
     if getattr(hps.data, "disable_bert", False):
         bert = torch.zeros(1024, len(phone))
         ja_bert = torch.zeros(768, len(phone))
     else:
-        # Move interspersing BEFORE BERT
-        if hps.data.add_blank:
-            phone = commons.intersperse(phone, 0)
-            tone = commons.intersperse(tone, 0)
-            language = commons.intersperse(language, 0)
-            for i in range(len(word2ph)):
-                word2ph[i] = word2ph[i] * 2
-            word2ph[0] += 1
-
-    bert = get_bert(norm_text, word2ph, language_str, device)
-    del word2ph
-    assert bert.shape[-1] == len(phone), f"BERT len {bert.shape[-1]} vs phone len {len(phone)}"
+        bert = get_bert(norm_text, word2ph, language_str, device)
+        del word2ph
+        assert bert.shape[-1] == len(phone), f"BERT len {bert.shape[-1]} vs phone len {len(phone)}"
 
         if language_str == "ZH":
-            bert = bert
             ja_bert = torch.zeros(768, len(phone))
         elif language_str in ["JP", "EN", "ZH_MIX_EN", "KR", "SP", "ES", "FR", "DE", "RU", "RO"]:
             ja_bert = bert
             bert = torch.zeros(1024, len(phone))
-
         else:
             raise NotImplementedError()
 
-    assert bert.shape[-1] == len(
-        phone
-    ), f"Bert seq len {bert.shape[-1]} != {len(phone)}"
+    assert bert.shape[-1] == len(phone), f"Bert seq len {bert.shape[-1]} != {len(phone)}"
 
     phone = torch.LongTensor(phone)
     tone = torch.LongTensor(tone)
